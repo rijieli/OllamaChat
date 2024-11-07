f30106edc3e525c8e1492d077ef0579fd535c71a
Roger <rijie.li@outlook.com>
2024-11-07 14:34:00 +0800

    Fix main window wrong on launch

--- OllamaChat/OllamaChatApp.swift
+++ OllamaChat/OllamaChatApp.swift
@@ -14,37 +14,29 @@ struct OllamaChatApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     var body: some Scene {
-        let mainWindow = WindowGroup {
+        Settings {
+          SettingsView()
+        }
+        WindowGroup {
             ContentView()
                 .preferredColorScheme(.light)
                 .environment(\.managedObjectContext, CoreDataStack.shared.context)
-          }
-          #if os(macOS)      
-          Settings {
-            SettingsView()
-          }
-          mainWindow.commands {
+        }.commands {
             CommandGroup(after: .newItem) {
-              Button(action: {
-                if let currentWindow = NSApp.keyWindow,
-                  let windowController = currentWindow.windowController {
-                  windowController.newWindowForTab(nil)
-                  if let newWindow = NSApp.keyWindow,
-                    currentWindow != newWindow {
-                      currentWindow.addTabbedWindow(newWindow, ordered: .above)
+                Button(action: {
+                    if let currentWindow = NSApp.keyWindow,
+                       let windowController = currentWindow.windowController {
+                        windowController.newWindowForTab(nil)
+                        if let newWindow = NSApp.keyWindow,
+                           currentWindow != newWindow {
+                            currentWindow.addTabbedWindow(newWindow, ordered: .above)
+                        }
                     }
+                }) {
+                    Text("New Tab")
                 }
-              }) {
-                Text("New Tab")
-              }
-              .keyboardShortcut("t", modifiers: [.command])
+                .keyboardShortcut("t", modifiers: [.command])
             }
-          }
-          #else
-          mainWindow
-          #endif
-//        WindowGroup {
-//            ContentView()
-//        }
+        }
     }
 }
