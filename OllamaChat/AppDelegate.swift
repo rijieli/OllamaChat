3c8dd545e78ea1e5cb91e25c4d850eca01718c8a
Roger <rijie.li@outlook.com>
2025-02-17 14:05:13 +0800

    Integrate Sparkle

--- OllamaChat/AppDelegate.swift
+++ OllamaChat/AppDelegate.swift
@@ -9,19 +9,35 @@
 import Foundation
 #if os(macOS)
 import AppKit
+import Sparkle
 #else
 import UIKit
 #endif
 import SwiftUI
 
+class AppDelegate: NSObject {
+    #if os(macOS)
+    var updaterController: SPUStandardUpdaterController?
+    #endif
+}
+
 #if os(macOS)
-class AppDelegate: NSObject, NSApplicationDelegate {
+extension AppDelegate: NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Initialize Sparkle updater
+        updaterController = SPUStandardUpdaterController(
+            startingUpdater: true,
+            updaterDelegate: nil,
+            userDriverDelegate: nil
+        )
+    }
+
     func applicationWillTerminate(_ notification: Notification) {
         CoreDataStack.shared.saveContext()
     }
 }
 #else
-class AppDelegate: NSObject, UIApplicationDelegate {
+extension AppDelegate: UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         CoreDataStack.shared.saveContext()
     }
